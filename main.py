6fd61cca2c405f32281b35f22467c89301d479e4
YashchenkoBV <94622784+YashchenkoBV@users.noreply.github.com>
2022-04-17 19:50:30 +0300

    Почти все готово

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ TOKEN = '5278858507:AAFA_jQaFD8oSFzMqyp3G5e0mjqS-hYkwT4'
 WEATHER_TOKEN = 'a911cb6b-7f4b-4b40-99b4-a1a8f235ad78'
 current_name = ''
 user_keyboard = [['/registration', '/enter']]
-functions_keyboard = [['/advice', '/weather_conditions', '/weather', '/link'], ['change_city']]
+functions_keyboard = [['/advice', '/weather_conditions', '/weather', '/link'], ['/change_city']]
 user_markup = ReplyKeyboardMarkup(user_keyboard, one_time_keyboard=True)
 functional_markup = ReplyKeyboardMarkup(functions_keyboard)
 
 
 def registration(update, context):
-    update.message.reply_text('''Вы активировали процесс регистрации. Чтобы прервать последующий диалог,
-используйте команду /stop. Пожалуйста, введите свой никнейм''')
+    update.message.reply_text('Вы активировали процесс регистрации. Чтобы прервать последующий диалог,'
+                              'используйте команду /stop. Пожалуйста, введите свой никнейм')
     return 1
 
 
@@ -26,6 +26,9 @@ def registration_name(update, context):
     context.user_data['new_name'] = update.message.text
     db_session.global_init("db/blogs.db")
     db_sess = db_session.create_session()
+    if context.user_data['new_name'] == '---':
+        update.message.reply_text('Пожалуйста, придумайте другое имя')
+        return 1
     for user in db_sess.query(User).all():
         if user.name == context.user_data['new_name']:
             update.message.reply_text('Пользователь с таким именем уже существет. '
@@ -45,7 +48,7 @@ def geolocation(city: str):
 def registration_password(update, context):
     password = update.message.text
     context.user_data['password'] = password
-    update.message.reply_text('Отлично, все почти готово'
+    update.message.reply_text('Отлично, все почти готово. '
                               'Теперь введите Ваш город проживания')
 
     return 3
@@ -90,31 +93,48 @@ def start(update, context):
 
 
 def functional(update, context):
-    update.message.reply_text(f'Добро пожаловать, {context.user_data["name"]}!'
+    db_session.global_init("db/cities.db")
+    db_sess = db_session.create_session()
+    city = ''
+    for user in db_sess.query(User).all():
+        if user.name == context.user_data["name"]:
+            city = user.constant_city
+            break
+    update.message.reply_text(f'Добро пожаловать, {context.user_data["name"]}! '
                               'Вам доступны следующие функции:', reply_markup=functional_markup)
+    context.user_data['current_area'] = city
+    latitude, longitude = geolocation(context.user_data['current_area'])
+    yandex_weather_x = yandex_weather(latitude, longitude, WEATHER_TOKEN)
+    context.user_data['link'] = yandex_weather_x['link']
+    print_weather(yandex_weather_x, update, context)
 
 
 def change_city(update, context):
-    update.message.reply_text('Хотите поменять город проживания?'
-                              'Хорошо, введите новое место:')
-    return 1
+    if context.user_data['name'] != '---':
+        update.message.reply_text('Хотите поменять город проживания?'
+                                  'Хорошо, введите новое место:')
+        return 1
+    else:
+        update.message.reply_text('Сначала войдите в систему')
+        return ConversationHandler.END
 
 
 def change_city_handling(update, context):
     new_city = update.message.text
     db_session.global_init("db/cities.db")
     db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.name == context.user_data['name']).all
-    user.constant_city = new_city
-    db_sess.commit()
+    context.user_data['current_area'] = new_city
+    for user in db_sess.query(User).all():
+        if user.name == context.user_data["name"]:
+            user.constant_city = new_city
+            db_sess.commit()
     update.message.reply_text('Город проживания успешно изменен')
     return ConversationHandler.END
 
 
-
 def enter(update, context):
-    update.message.reply_text('''Вы активировали процесс входа. Чтобы прервать последующий диалог,
-    используйте команду /stop. Пожалуйста, введите свой никнейм''')
+    update.message.reply_text('Вы активировали процесс входа. Чтобы прервать последующий диалог, '
+                              'используйте команду /stop. Пожалуйста, введите свой никнейм')
     return 1
 
 
@@ -173,7 +193,6 @@ def yandex_weather(latitude, longitude, token):
     wind_dir = {'nw': 'северо-западное', 'n': 'северное', 'ne': 'северо-восточное', 'e': 'восточное',
                 'se': 'юго-восточное', 's': 'южное', 'sw': 'юго-западное', 'w': 'западное', 'c': 'штиль'}
 
-    season = {'summer': 'лето', 'autumn': 'осень', 'winter': 'зима', 'spring': 'весна'}
     yandex_json = json.loads(yandex_req.text)
     yandex_json['fact']['condition'] = conditions[yandex_json['fact']['condition']]
     yandex_json['fact']['wind_dir'] = wind_dir[yandex_json['fact']['wind_dir']]
@@ -182,53 +201,136 @@ def yandex_weather(latitude, longitude, token):
         parts['wind_dir'] = wind_dir[parts['wind_dir']]
 
     weather = dict()
-    params = ['condition', 'wind_dir', 'pressure_mm', 'humidity']
+    fact_params = ['temp', 'feels_like', 'condition', 'wind_dir',
+                   'wind_speed', 'wind_gust', 'pressure_mm', 'humidity']
+    forecast_params = ['sunrise', 'sunset']
+    parts_params = ['temp_avg', 'feels_like',
+                    'condition', 'wind_speed', 'wind_gust', 'wind_dir',
+                    'pressure_mm', 'humidity', 'prec_prob']
+
     for parts in yandex_json['forecast']['parts']:
         weather[parts['part_name']] = dict()
-        weather[parts['part_name']]['temp'] = parts['temp_avg']
-        for param in params:
+        for param in parts_params:
             weather[parts['part_name']][param] = parts[param]
 
     weather['fact'] = dict()
-    weather['fact']['temp'] = yandex_json['fact']['temp']
-    for param in params:
+    for param in fact_params:
         weather['fact'][param] = yandex_json['fact'][param]
 
+    weather['forecast'] = dict()
+    for param in forecast_params:
+        weather['forecast'][param] = yandex_json['forecast'][param]
+
     weather['link'] = yandex_json['info']['url']
+
     print(weather)
+    print(weather.keys())
     return weather
 
 
 def link(update, context):
-    cur_area = context.user_data['current_area']
-    if cur_area:
-        latitude, longitude = geolocation(cur_area)
-        url_yandex = f'https://api.weather.yandex.ru/v2/informers?lat={latitude}&lon={longitude}&[lang=ru_RU]'
-        update.message.reply_text(f'Вот ссылка на боле подробную информацию о погоде:\n{url_yandex}')
+    if context.user_data['name'] != '---':
+        update.message.reply_text('Вот ссылка на подробности:'
+                                  f'{context.user_data["link"]}')
     else:
-        update.message.reply_text('Для начала запросите информацию о погоде в каком-либо городе')
+        update.message.reply_text('Сначала войдите в систему')
 
 
 def print_weather(dict_weather_yandex, update, context):
-    day = {'night': 'ночью', 'morning': 'утром', 'day': 'днем', 'evening': 'вечером', 'fact': 'сейчас'}
     update.message.reply_text(f'Погода в городе {context.user_data["current_area"]} на данный момент:\n'
-                              f'Температура: {dict_weather_yandex["fact"]["temp"]}\n'
+                              f'Температура воздуха: {dict_weather_yandex["fact"]["temp"]}℃\n'
                               f'Направление ветра {dict_weather_yandex["fact"]["wind_dir"]}\n'
-                              f'Влажность воздуха: {dict_weather_yandex["fact"]["humidity"]}%')
+                              f'Влажность воздуха: {dict_weather_yandex["fact"]["humidity"]}%\n'
+                              f'{dict_weather_yandex["fact"]["condition"]}')
 
 
 def main_weather(update, context):
     global WEATHER_TOKEN
     context.user_data['current_area'] = update.message.text
     latitude, longitude = geolocation(context.user_data['current_area'])
-    print(latitude, longitude)
     yandex_weather_x = yandex_weather(latitude, longitude, WEATHER_TOKEN)
+    context.user_data['link'] = yandex_weather_x['link']
     print_weather(yandex_weather_x, update, context)
+    print(context.user_data)
+    return ConversationHandler.END
 
 
 def weather(update, context):
-    update.message.reply_text('Хотите узнать погоду? Введите название интересующего города:')
-    return 1
+    if context.user_data['name'] != '---':
+        update.message.reply_text('Хотите узнать погоду? Введите название интересующего города:')
+        return 1
+    else:
+        update.message.reply_text('Сначала войдите в систему')
+        return ConversationHandler.END
+
+
+def weather_conditions(update, context):
+    if context.user_data['name'] != '---':
+        update.message.reply_text('Введите название города, для которого хотите '
+                                  'узнать подробную информацию о погоде')
+        return 1
+    else:
+        update.message.reply_text('Сначала войдите в систему')
+        return ConversationHandler.END
+
+
+def detailed_weather(update, context):
+    global WEATHER_TOKEN
+    context.user_data['current_area'] = update.message.text
+    latitude, longitude = geolocation(context.user_data['current_area'])
+    yandex_weather_x = yandex_weather(latitude, longitude, WEATHER_TOKEN)
+    context.user_data['link'] = yandex_weather_x['link']
+    print_detailed_weather(yandex_weather_x, update, context)
+    return 2
+
+
+def print_detailed_weather(dict_weather, update, context):
+    update.message.reply_text(f'Погода в городе {context.user_data["current_area"]} на данный момент:\n'
+                              f'Температура воздуха: {dict_weather["fact"]["temp"]}℃'
+                              f'(ощущается как {dict_weather["fact"]["feels_like"]}℃)\n'
+                              f'Направление ветра {dict_weather["fact"]["wind_dir"]}\n'
+                              f'Скорость ветра {dict_weather["fact"]["wind_speed"]} м/с\n'
+                              f'Скорость порывов ветра {dict_weather["fact"]["wind_gust"]} м/с\n'
+                              f'Влажность воздуха: {dict_weather["fact"]["humidity"]}%\n'
+                              f'Давление {dict_weather["fact"]["pressure_mm"]} мм рт. ст.\n'
+                              f'{dict_weather["fact"]["condition"]}\n'
+                              f'Время рассвета:{dict_weather["forecast"]["sunrise"]}\n'
+                              f'Время заката:{dict_weather["forecast"]["sunset"]}\n'
+                              'Для того чтобы узнать прогноз на утро/день/вечер/ночь, '
+                              'напишите боту morning/day/evening/night соответственно. '
+                              'Если Вам это не нужно, воспользуйтесь командой /stop'
+                              )
+
+
+def daytime_weather(update, context):
+    day = {'night': 'ночью', 'morning': 'утром', 'day': 'днем', 'evening': 'вечером'}
+    global WEATHER_TOKEN
+    latitude, longitude = geolocation(context.user_data['current_area'])
+    yandex_weather_x = yandex_weather(latitude, longitude, WEATHER_TOKEN)
+    daytime = update.message.text
+    if daytime not in yandex_weather_x.keys():
+        update.message.reply_text('Увы, введенное Вами время дня уже прошло (или вы ввели его некорректно)')
+        return ConversationHandler.END
+    else:
+        for key in yandex_weather_x:
+            if key == daytime:
+                update.message.reply_text(f'Погода в городе {context.user_data["current_area"]} {day[daytime]}:\n'
+                                          f'Средняя температура {yandex_weather_x[daytime]["temp_avg"]}℃\n'
+                                          f'Ощущается как {yandex_weather_x[daytime]["feels_like"]}℃\n'
+                                          f'Скорость ветра {yandex_weather_x[daytime]["wind_speed"]} м/с\n'
+                                          f'Скорость порывов ветра {yandex_weather_x[daytime]["wind_gust"]} м/с\n'
+                                          f'Направление ветра {yandex_weather_x[daytime]["wind_dir"]}\n'
+                                          f'Давление {yandex_weather_x[daytime]["pressure_mm"]} мм рт. ст.\n'
+                                          f'Влажность воздуха {yandex_weather_x[daytime]["humidity"]} %\n'
+                                          f'Вероятность осадков {yandex_weather_x[daytime]["prec_prob"]} %\n'
+                                          f'{yandex_weather_x[daytime]["condition"]}')
+                break
+        return ConversationHandler.END
+
+
+def quit(update, context):
+    context.user_data['name'] = '---'
+    update.message.reply_text('Вы вышли из системы. Чтобы продолжить работу, пожалуйста, войдите заново.')
 
 
 def main():
@@ -237,8 +339,9 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', start, pass_user_data=True))
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(CommandHandler('link', link))
-    dp.add_handler(CommandHandler('change_city', change_city))
+    dp.add_handler(CommandHandler('link', link, pass_user_data=True))
+    dp.add_handler(CommandHandler('quit', quit, pass_user_data=True))
+
     conv_handler1 = ConversationHandler(
         entry_points=[CommandHandler('registration', registration)],
         states={
@@ -248,6 +351,7 @@ def main():
         },
         fallbacks=[CommandHandler('stop', stop)]
     )
+
     conv_handler2 = ConversationHandler(
         entry_points=[CommandHandler('enter', enter)],
         states={
@@ -255,22 +359,34 @@ def main():
             2: [MessageHandler(Filters.text & ~Filters.command, enter_password, pass_user_data=True)]
         },
         fallbacks=[CommandHandler('stop', stop)])
+
     conv_handler3 = ConversationHandler(
         entry_points=[CommandHandler('weather', weather)],
         states={
             1: [MessageHandler(Filters.text & ~Filters.command, main_weather, pass_user_data=True)]
         },
         fallbacks=[CommandHandler('stop', stop)])
+
     conv_handler4 = ConversationHandler(
         entry_points=[CommandHandler('change_city', change_city)],
         states={
             1: [MessageHandler(Filters.text & ~Filters.command, change_city_handling, pass_user_data=True)]
         },
         fallbacks=[CommandHandler('stop', stop)])
+
+    conv_handler5 = ConversationHandler(
+        entry_points=[CommandHandler('weather_conditions', weather_conditions, pass_user_data=True)],
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, detailed_weather, pass_user_data=True)],
+            2: [MessageHandler(Filters.text & ~Filters.command, daytime_weather, pass_user_data=True)]
+        },
+        fallbacks=[CommandHandler('stop', stop)])
+
     dp.add_handler(conv_handler1)
     dp.add_handler(conv_handler2)
     dp.add_handler(conv_handler3)
     dp.add_handler(conv_handler4)
+    dp.add_handler(conv_handler5)
     updater.start_polling()
 
     updater.idle()
